17423fc0e1dfe5ab95bbe52590339f057d3c070f
Александр <alexgalimov2003@mail.ru>
2024-09-14 18:35:41 +0300

    NIR v01

--- src/main/java/cs/vsu/ru/galimov/tasks/articleviewerextractorservice/components/Extractor.java
+++ src/main/java/cs/vsu/ru/galimov/tasks/articleviewerextractorservice/components/Extractor.java
@@ -39,13 +39,11 @@ public class Extractor {
         }
         List<Article> articles;
 
-        for (int i = 0; i < departmentMagazines.size(); i++) {
-            List<DateArchive> dateArchives = new ArrayList<>();
+        for (int i = 0; i < 1; i++) {
             for (Archive archive: departmentMagazines.get(i).getArchives()) {
                 List<DateArchive> currDateArchives = findDateArchives(archive, magazinesURLs.get(i));
                 articles = makeArticles(departmentMagazines.get(i), currDateArchives, archive);
                 articleSender.sendMessagesToKafka(articles);
-                Thread.sleep(5000);
             }
         }
     }
